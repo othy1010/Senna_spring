6b4104f09e3dcb81dbb9f3835426776c5a74211f
othy1010 <othmane.elkarmy@um5r.ac.ma>
2022-06-22 13:30:30 +0100

    add firebase support login

--- senna_front/components/Header.js
+++ senna_front/components/Header.js
@@ -178,20 +178,7 @@ export default function Header() {
                            </Popover.Button>
                         </div>
                      </div>
-                     <div className="mt-6">
-                        <nav className="grid gap-y-8">
-                           {drugs.map((item) => (
-                              <a
-                                 key={item.name}
-                                 href={item.href}
-                                 className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                              >
-                                 <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                                 <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                              </a>
-                           ))}
-                        </nav>
-                     </div>
+
                   </div>
                   <div className="py-6 px-5 space-y-6">
                      <div className="grid grid-cols-2 gap-y-4 gap-x-8">
