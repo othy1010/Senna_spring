650ce7cf23a981e9cf260fce23cfc86a436c6c9d
othy1010 <othmane.elkarmy@um5r.ac.ma>
2022-06-28 21:12:46 +0100

    update crud

--- senna_front/pages/api/DrugService.js
+++ senna_front/pages/api/DrugService.js
@@ -2,70 +2,90 @@ import { useRouter } from 'next/router'
 import axios from 'axios';
 
 
-const bk_url = process.env.BACKEND_URL
-// const bk_url = "http://localhost:8080"
-const DRUGS_REST_API_URL = "http://backend:8080/api/drugs";
 class DrugService {
-  //drugs
-
   async getDrugs() {
-
     try {
       const response = await fetch(DRUGS_REST_API_URL)
-      console.log(response, "⚡")
-      return await response;
+      return response;
     }
     catch (err) {
       console.log(err)
       throw (err)
     }
-
   }
   async getDrugById(drugId) {
     try {
-
-      const response = await axios.get(`http://backend:8080/api/drugs/drugId/${drugId}`);
-      console.log(response, "⚡getDrugById⚡")
+      const response = await axios.get(`http://backend:8080/api/drugs/drugId/${drugId}`)
       return response;
     }
     catch (err) {
-      console.log(err, "🚚🚒🚒🚑🚛🚚🛴")
-      console.log(err.request, "💥💌💌💌💥💢")
+      console.log(err, "🚚")
+      console.log(err.request, "💥")
       throw (err)
     }
   }
   async getDrugByPatientId(patientId) {
-    const response = await fetch(DRUGS_REST_API_URL + "/patientId/" + patientId).catch(err => console.log(err));
-    console.log(bk_url, "getDrugByPatientId")
-    return await response.json();
+    try {
+      const response = await axios.get(`http://backend:8080/api/drugs/patientId/${patientId}`)
+      return response;
+    }
+    catch (err) {
+      console.log(err, "🚚")
+      console.log(err.request, "💥")
+      throw (err)
+    }
   }
   async getDrugByDrugUsage(drugUsage) {
-    const response = await fetch(DRUGS_REST_API_URL + "/drugUsage/" + drugUsage).catch(err => console.log(err));
-    console.log(bk_url, "getDrugByDrugUsage")
-    return await response.json();
+    try {
+
+      const response = await axios.get(`http://backend:8080/api/drugs/drugUsage/${drugUsage}`)
+      return response;
+    }
+    catch (err) {
+      console.log(err, "🚚")
+      console.log(err.request, "💥")
+      throw (err)
+    }
   }
   async getDrugByDrugName(drugName) {
-    const response = await fetch(DRUGS_REST_API_URL + "/drugName/" + drugName).catch(err => console.log(err));
-    console.log(bk_url, "getDrugByDrugName")
-    return await response.json();
+    try {
+      const response = await axios.get(`http://backend:8080/api/drugs/drugName/${drugName}`)
+      return response;
+    }
+    catch (err) {
+      console.log(err, "🚚")
+      console.log(err.request, "💥")
+      throw (err)
+    }
   }
   async getDrugByDrugSupplierId(drugSupplierId) {
-    const response = await fetch(DRUGS_REST_API_URL + "/drugSupplierId/" + drugSupplierId).catch(err => console.log(err));
-    console.log(bk_url, "getDrugByDrugSupplierId")
-    return await response.json();
+    try {
+      const response = await axios.get(`http://backend:8080/api/drugs/drugSupplierId/${drugSupplierId}`)
+      return response;
+    }
+    catch (err) {
+      console.log(err, "🚚")
+      console.log(err.request, "💥")
+      throw (err)
+    }
   }
   async createDrug(drug) {
-    const response = await fetch(DRUGS_REST_API_URL, { method: "POST", body: JSON.stringify(drug), headers: { "Content-Type": "application/json" } }).catch(err => console.log(err));
-    console.log(response, "post_request_drug")
-    return await response.json();
-    // return axios.post(DRUGS_REST_API_URL, drug);
+    try {
+      await axios.post(`/api/drugs/`, drug);
+    }
+    catch (err) {
+      console.log(err)
+      throw (err)
+    }
   }
-
   async updateDrug(drugId, drug) {
-    const response = await fetch(DRUGS_REST_API_URL + "/drugId/" + drugId, { method: "PUT", body: JSON.stringify(drug), headers: { "Content-Type": "application/json" } }).catch(err => console.log(err));
-    console.log(response, "update_request_drug")
-    return await response.json();
-
+    try {
+      await axios.post(`/api/drugs/drugId/${drugId}`, drug);
+    }
+    catch (err) {
+      console.log(err)
+      throw (err)
+    }
   }
   async deleteDrug(drugId) {
     try {
@@ -76,16 +96,6 @@ class DrugService {
       console.log(err)
       throw (err)
     }
-
-    // try {
-    //   const response = await fetch(DRUGS_REST_API_URL + "/drugId/" + drugId, { method: "DELETE" })
-    //   console.log(response, "⚡delete_request_drug⚡")
-    // }
-    // catch (err) {
-    //   console.log(err)
-    //   throw (err)
-    // }
-
   }
 
 }
